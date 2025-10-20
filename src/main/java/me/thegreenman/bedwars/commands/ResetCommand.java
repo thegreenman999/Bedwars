b1271a356406d374b0e947c31d81ce84c4712de4
thegreenman999 <thegreenman9999@gmail.com>
2025-10-20 16:38:02 +0200

    Adde reseting of chests

--- src/main/java/me/thegreenman/bedwars/commands/ResetCommand.java
+++ src/main/java/me/thegreenman/bedwars/commands/ResetCommand.java
@@ -2,8 +2,13 @@ package me.thegreenman.bedwars.commands;
 
 import me.thegreenman.bedwars.Bedwars;
 
+import me.thegreenman.bedwars.utils.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,6 +17,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
+import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -49,6 +55,7 @@ public class ResetCommand implements CommandExecutor, Listener {
     }
 
     public static void reset() {
+        clearChests();
 
         if (placeBlock.isEmpty()) {
             return;
@@ -60,8 +67,41 @@ public class ResetCommand implements CommandExecutor, Listener {
                 block.setType(Material.AIR);
             }
         }
-        // TODO: clear all chest in the arena
+
         placeBlock.removeAll(toRemove);
+    }
+
+    private static void clearChests() {
+        for (int i = 0; i < 4; i++) {
+            String team = switch (i) {
+                case 0 -> "RedTeam";
+                case 1 -> "PinkTeam";
+                case 2 -> "BlueTeam";
+                case 3 -> "GreenTeam";
+                default -> throw new IllegalStateException("Unexpected value: " + i);
+            };
+
+            Location loc = new Location(
+                    Bedwars.world,
+                    Bedwars.gameConfig.getInt(team + ".chest-loc.x"),
+                    Bedwars.gameConfig.getInt(team + ".chest-loc.y"),
+                    Bedwars.gameConfig.getInt(team + ".chest-loc.z"));
+
+            loc.getBlock().setType(Material.AIR);
+            loc.getBlock().setType(Material.CHEST);
+
+//            if (!(block.getState() instanceof Chest chest)) {
+//                return;
+//            }
+//
+//            Inventory inv = chest.getInventory();
+//            inv.clear();
+//            chest.update();
+
+//            if (Bedwars.debug) {
+//                Logger.log("chest at " + chest.getLocation() + " has bin cleared");
+//            }
+        }
     }
 
 }
